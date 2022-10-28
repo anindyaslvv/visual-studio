99a56ac22cba158062d35c00851c3951e4fd4ece
anindyaslvv <116860011+anindyaslvv@users.noreply.github.com>
2022-10-27 23:00:49 -0700

    Add files via upload

---
+++ tugas/tugas/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+int jamkerja = 10;
+int hargaperjam = 15000;
+int gajidibayar;
+int l;
+
+//cek kondisi jika lebih dari 7 jam 
+if (jamkerja > 7)
+{
+    //proses gaji dibayar 
+    l = jamkerja - 7;
+    if (l == 1)
+    {
+        gajidibayar = (hargaperjam * jamkerja) + 10000;
+    }
+    else
+    {
+        gajidibayar = (hargaperjam * jamkerja);
+    }
+    //proses gaji dibayar 
+    l = jamkerja - 7;
+    if (l == 2)
+    {
+        gajidibayar = (hargaperjam * jamkerja) + 20000;
+    }
+    else
+    {
+        gajidibayar = (hargaperjam * jamkerja);
+    }
+    //proses gaji dibayar
+    l = jamkerja - 7;
+    if (l == 3)
+    {
+        gajidibayar = (hargaperjam * jamkerja) + 30000;
+    }
+    else
+    {
+        gajidibayar = (hargaperjam * jamkerja);
+    }
+    //proses gaji dibayar 
+    l = jamkerja - 7;
+    if (l == 4)
+    {
+        gajidibayar = (hargaperjam * jamkerja) + 100000;
+    }
+    else
+    {
+        gajidibayar = (hargaperjam * jamkerja);
+    }
+    //jika normal 
+
+    Console.WriteLine("tampilkan gaji dibayar : " + gajidibayar);
+    Console.ReadKey();
+}
+
+
+
+
+
+
+
